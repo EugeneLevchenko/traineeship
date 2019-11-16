5ebfa6bdb50c5e4a377cea18685da53eb1906c51
EugeneLevchenko <eugene140823@gmail.com>
2019-11-16 19:03:32 +0200

    b_bytecode

---
+++ module1/src/main/java/net/broscorp/b_bytecode/App.java
@@ -0,0 +1,17 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        for (int i=0;i<5;i++)
+        {
+           i++;
+        }
+        App.experimentalMethod();
+        System.out.println("Hi Eugene");
+    }
+
+    private static void experimentalMethod()
+    {
+
+    }
+}
